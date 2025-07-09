2e0475d0bbbbe8370ef458af8e600dfad51a15ad
jyotirmay-neuro <101052218+jyotirmay-neuro@users.noreply.github.com>
2025-07-09 11:46:58 +0530

    postgraduation: Fix-2 post-graduation journey details

--- content/journey/postgraduation.md
+++ content/journey/postgraduation.md
@@ -7,4 +7,4 @@ date: "2024-07-22"
 ![MS Diploma](/images/journey/MS.jpg)
 I just graduated with a Master of Science by Research in Biological Sciences Degree from IISER Thiruvananthapuram! Though my journey at this phenomenal place was cut short, receiving this degree is a reminder of all things great I learnt and achieved at this gem of an institution. The place, the people, the weather, just makes you feel at home. Cannot thank the professors, and everyone, enough for these 4 years of heaven.
 
-On a different note, I am now **on the lookout for a research assistant or PhD position.** Any and all leads would be greatly appreciated.
+On a different note, I am now on the lookout for a research assistant or PhD position. Any and all leads would be greatly appreciated.
--- src/data/journeyData.ts
+++ src/data/journeyData.ts
@@ -26,7 +26,7 @@ export const journeyPosts: JourneyPost[] = [
     "date": "2024-07-22",
     "type": "text",
     "title": "Graduated Masters!",
-    "content": "I just graduated with a Master of Science by Research in Biological Sciences Degree from IISER Thiruvananthapuram! Though my journey at this phenomenal place was cut short, receiving this degree is a reminder of all things great I learnt and achieved at this gem of an institution. The place, the people, the weather, just makes you feel at home. Cannot thank the professors, and everyone, enough for these 4 years of heaven.\r\n\r\nOn a different note, I am now **on the lookout for a research assistant or PhD position.** Any and all leads would be greatly appreciated.",
+    "content": "I just graduated with a Master of Science by Research in Biological Sciences Degree from IISER Thiruvananthapuram! Though my journey at this phenomenal place was cut short, receiving this degree is a reminder of all things great I learnt and achieved at this gem of an institution. The place, the people, the weather, just makes you feel at home. Cannot thank the professors, and everyone, enough for these 4 years of heaven.\r\n\r\nOn a different note, I am now on the lookout for a research assistant or PhD position. Any and all leads would be greatly appreciated.",
     "imagePath": "/images/journey/MS.jpg",
     "imageAlt": "MS Diploma"
   },
